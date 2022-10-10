1f5f1a8ea0674f6529e1e18e29f1bd0d43fad260
Sean <craig.sean92@gmail.com>
2022-10-10 12:53:12 -0600

    palette api connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [data, setData] = useState()
+  console.log(data)
+
+  useEffect(() => {
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({ 	
+        model : "default",
+        input : ["N","N","N","N","N"]})
+    }
+    fetch(`http://colormind.io/api/`, options)
+      .then((r) => r.json())
+      .then((data) => setData(data))
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>hello</div>
   );
 }
 
